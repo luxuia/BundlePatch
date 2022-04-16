ed4c61cc866cf170826c44ee11a250f44c038582
luxuia <luxuia@foxmail.com>
2022-04-16 17:48:28 +0800

    add assetstudio lib

---
+++ BundlePatch/AssetStudio/EndianBinaryWriter.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+
+namespace AssetStudio
+{
+    public class EndianBinaryWriter : BinaryWriter
+    {
+        private readonly byte[] buffer;
+
+        public EndianType Endian;
+
+        public EndianBinaryWriter(Stream stream, EndianType endian = EndianType.BigEndian) : base(stream)
+        {
+            Endian = endian;
+            buffer = new byte[8];
+        }
+
+        public long Position
+        {
+            get => BaseStream.Position;
+            set => BaseStream.Position = value;
+        }
+
+        public override void Write(short value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                BinaryPrimitives.WriteInt16BigEndian(buffer, value);
+                Write(buffer, 0, 2);
+                return;
+            }
+            base.Write(value);
+        }
+
+        public override void Write(int value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                 BinaryPrimitives.WriteInt32BigEndian(buffer, value);
+
+                Write(buffer, 0, 4);
+
+                return;
+            }
+            base.Write(value);
+        }
+
+        public override void Write(long value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                BinaryPrimitives.WriteInt64BigEndian(buffer, value);
+
+                Write(buffer, 0, 8);
+                return;
+            }
+             base.Write(value);
+        }
+
+        public override void Write(ushort value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                 BinaryPrimitives.WriteUInt16BigEndian(buffer, value);
+
+                Write(buffer, 0, 2);
+                return;
+            }
+            base.Write(value);
+        }
+
+        public override void Write(uint value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                BinaryPrimitives.WriteUInt32BigEndian(buffer, value);
+
+                Write(buffer, 0, 4);
+                return;
+            }
+            base.Write(value);
+        }
+
+        public override void Write(ulong value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                BinaryPrimitives.WriteUInt64BigEndian(buffer, value);
+
+                Write(buffer, 0, 8); ;
+                return;
+            }
+            Write(value);
+        }
+
+        public override void Write(float value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                BinaryPrimitives.WriteSingleBigEndian(buffer, value);
+                Write(buffer, 0, 4);
+                return;
+            }
+            base.Write(value);
+        }
+
+        public override void Write(double value)
+        {
+            if (Endian == EndianType.BigEndian)
+            {
+                BinaryPrimitives.WriteDoubleBigEndian(buffer, value);
+                Write(buffer, 0, 8);
+                return;
+            }
+            base.Write(value);
+        }
+    }
+}
---
+++ BundlePatch/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using AssetStudio;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static Dictionary<long, NamedObject> PatchObjs = new Dictionary<long, NamedObject>();
+        static Dictionary<long, NamedObject> OldObjs = new Dictionary<long, NamedObject>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var oldpath = "/Users/luxuia/Documents/Demo/Assets/tex";
+
+            var assetsmanager = new AssetsManager();
+            assetsmanager.LoadFiles(new string[] {oldpath});
+
+            var newpath = "/Users/luxuia/Documents/Demo/Assets/tex_new";
+            var patchassetsmgr = new AssetsManager();
+            patchassetsmgr.LoadFiles(new string[] { newpath });
+
+            foreach (var assetFile in patchassetsmgr.assetsFileList)
+            {
+                foreach (var obj in assetFile.Objects)
+                {
+             
+                    PatchObjs.Add(obj.m_PathID, (NamedObject)obj);
+                }
+            }
+
+
+            var topatchlist = new List<NamedObject>();
+            foreach(var assetFile in assetsmanager.assetsFileList)
+            {
+                foreach ( var obj in assetFile.Objects)
+                {
+                    if (PatchObjs.ContainsKey(obj.m_PathID))
+                    {
+                        switch ( obj)
+                        {
+                            case Texture2D tex2d:
+                                Console.WriteLine("tex2d, name {0} pathid: {1} ", tex2d.m_Name, tex2d.m_PathID);
+                                topatchlist.Add(tex2d);
+                                break;
+                            case NamedObject nameobj:
+                                Console.WriteLine("tex2d, name {0} pathid: {1} ", nameobj.m_Name, nameobj.m_PathID);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+
+            var ret = "/Users/luxuia/Documents/Demo/Assets/tex_patched";
+
+            foreach (var obj in topatchlist)
+            {
+                switch (obj)
+                {
+                    case Texture2D tex2d:
+                        var filename = Path.GetFileName(tex2d.m_StreamData.path);
+                        var resblock = assetsmanager.resourceFileReaders[filename];
+                        //var offset = resblock.Position;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
